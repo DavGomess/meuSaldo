3024bd0510a094d0229199d716904892a5182b32
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-12-16 14:54:58 -0300

    fix(auth): proteger rota home e evitar renderização sem usuário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,28 @@ import Categorias from "./categorias/categorias";
 import Orcamento from "./orcamento/orcamentos";
 import ContasPagar from "./contasPagar/contasPagar";
 import Configuracoes from "./configuracoes/configuracoes";
-import { useSelected } from "@/contexts/SelectedContext";
+import { useSelected } from "../contexts/SelectedContext";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function Home() {
+const router = useRouter();
+const { user, loading } = useAuth();
+const { selected } = useSelected();  
 
-  const { selected } = useSelected();
+useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading || !user) return null;
 
   return (
-    <div className="main">
+    <div className="mainPage border-1 p-0 me-0">
           <MenuLateral />
-        <div className="container p-0">
+        <div className="mainContainer p-0 me-0 ">
           <Header/>       
           {selected === "dashboard" && <Dashboard />}
           {selected === "transacoes" && <Transacoes />}
